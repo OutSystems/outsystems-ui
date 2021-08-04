ea192076708bf0cf1058a4567bd58a1843e410e7
José Rio <jose.rio@hotmail.com>
2021-08-04 10:01:16 +0100

    fix name

--- src/scripts/osui.ts
+++ src/scripts/osui.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-namespace osuiDemo {
+namespace osui {
 	export function GetVersion(): any {
 		return OutSystems.osuiAPI.GetVersion();
 	}
