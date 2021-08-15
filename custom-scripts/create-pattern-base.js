44e8d5ea45ca4da7d02214bfa5a125e256f742d9
gbnm <goncalo.martins@outsystems.com>
2021-08-15 15:23:23 +0100

    Created new custom script for new patterns
    Created new custom script to create all base directories and files needed for the development of a new OS UI pattern.
    We can run it like:

---
+++ custom-scripts/create-pattern-base.js
@@ -0,0 +1,94 @@
+var shell = require('shelljs');
+
+if (!shell.which('git')) {
+	shell.echo('Sorry, this script requires git');
+	shell.exit(1);
+}
+
+const patternname = process.env.npm_config_patternname;
+
+shell.echo('Lets do this or pattern = ' + patternname);
+
+const scssTemplate = 'custom-scripts/templates/SCSS-Template.scss';
+const pattTemplate = 'custom-scripts/templates/Pattern-Template.ts';
+const pattConfigTemplate = 'custom-scripts/templates/Config-Template.ts';
+const apiTemplate = 'custom-scripts/templates/API-Template.ts';
+const interfaceTemplate = 'custom-scripts/templates/Interface-Template.ts';
+
+const folderPath = 'src/scripts/OSUIFramework/Pattern/' + patternname;
+const scssFolderPath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/scss';
+const scssFilePath =
+	'src/scripts/OSUIFramework/Pattern/' + patternname + '/scss/_' + patternname.toLowerCase() + '.scss';
+const pattFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/' + patternname + '.ts';
+const pattConfigFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/' + patternname + 'Config.ts';
+const interfaceFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/I' + patternname + '.ts';
+const pattAPIFilePath = 'src/scripts/OutSystems/OSUI/Patterns/' + patternname + 'API.ts';
+
+//check if folder exists
+if (shell.test('-e', folderPath)) {
+	shell.echo('Sorry, the folder already exists ' + folderPath);
+} else {
+	shell.mkdir(folderPath);
+	shell.echo('Created ' + folderPath);
+}
+
+//check if scss folder exists
+if (shell.test('-e', scssFolderPath)) {
+	shell.echo('Sorry, the folder already exists ' + scssFolderPath);
+} else {
+	shell.mkdir(scssFolderPath);
+	shell.echo('Created ' + scssFolderPath);
+}
+
+//check if scss file exists
+if (shell.test('-e', scssFilePath)) {
+	shell.echo('Sorry, the folder already exists ' + scssFilePath);
+} else {
+	shell.touch(scssFilePath);
+	shell.cat(scssTemplate).to(scssFilePath);
+	shell.sed('-i', '<Pattern-Name>', patternname, scssFilePath);
+
+	shell.echo('Created ' + scssFilePath);
+}
+
+//check if iterface file exists
+if (shell.test('-e', interfaceFilePath)) {
+	shell.echo('Sorry, the folder already exists ' + interfaceFilePath);
+} else {
+	shell.touch(interfaceFilePath);
+	shell.cat(interfaceTemplate).to(interfaceFilePath);
+	shell.sed('-i', '<Pattern-Name>', patternname, interfaceFilePath);
+	shell.echo('Created ' + interfaceFilePath);
+}
+
+//check if pattern ts file exists
+if (shell.test('-e', pattFilePath)) {
+	shell.echo('Sorry, the folder already exists ' + pattFilePath);
+} else {
+	shell.touch(pattFilePath);
+	shell.cat(pattTemplate).to(pattFilePath);
+	shell.sed('-i', '<Pattern-Name>', patternname, pattFilePath);
+	shell.sed('-i', '<Pattern-Name2>', patternname, pattFilePath);
+	shell.sed('-i', '<Pattern-Name3>', patternname, pattFilePath);
+	shell.echo('Created ' + pattFilePath);
+}
+
+//check if pattern API file exists
+if (shell.test('-e', pattAPIFilePath)) {
+	shell.echo('Sorry, the folder already exists ' + pattAPIFilePath);
+} else {
+	shell.touch(pattAPIFilePath);
+	shell.cat(apiTemplate).to(pattAPIFilePath);
+	shell.sed('-i', '<Pattern-Name>', patternname, pattAPIFilePath);
+	shell.echo('Created ' + pattAPIFilePath);
+}
+
+//check if pattern config file exists
+if (shell.test('-e', pattConfigFilePath)) {
+	shell.echo('Sorry, the folder already exists ' + pattConfigFilePath);
+} else {
+	shell.touch(pattConfigFilePath);
+	shell.cat(pattConfigTemplate).to(pattConfigFilePath);
+	shell.sed('-i', '<Pattern-Name>', patternname, pattConfigFilePath);
+	shell.echo('Created ' + pattConfigFilePath);
+}
