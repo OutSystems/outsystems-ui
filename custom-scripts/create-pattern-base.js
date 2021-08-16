1c38fe1fdb47a3c73069584b55e8aae5b8987fea
gbnm <goncalo.martins@outsystems.com>
2021-08-16 10:26:40 +0100

    Added the Enum.ts generation
    Added the Enum.ts generation

--- custom-scripts/create-pattern-base.js
+++ custom-scripts/create-pattern-base.js
@@ -14,6 +14,7 @@ const pattTemplate = 'custom-scripts/templates/Pattern-Template.ts';
 const pattConfigTemplate = 'custom-scripts/templates/Config-Template.ts';
 const apiTemplate = 'custom-scripts/templates/API-Template.ts';
 const interfaceTemplate = 'custom-scripts/templates/Interface-Template.ts';
+const enumTemplate = 'custom-scripts/templates/Enum-Template.ts';
 
 const folderPath = 'src/scripts/OSUIFramework/Pattern/' + patternname;
 const scssFolderPath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/scss';
@@ -21,12 +22,13 @@ const scssFilePath =
 	'src/scripts/OSUIFramework/Pattern/' + patternname + '/scss/_' + patternname.toLowerCase() + '.scss';
 const pattFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/' + patternname + '.ts';
 const pattConfigFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/' + patternname + 'Config.ts';
+const enumFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/Enum.ts';
 const interfaceFilePath = 'src/scripts/OSUIFramework/Pattern/' + patternname + '/I' + patternname + '.ts';
 const pattAPIFilePath = 'src/scripts/OutSystems/OSUI/Patterns/' + patternname + 'API.ts';
 
 //check if folder exists
 if (shell.test('-e', folderPath)) {
-	shell.echo('Sorry, the folder already exists ' + folderPath);
+	shell.echo('The folder already exists ' + folderPath);
 } else {
 	shell.mkdir(folderPath);
 	shell.echo('Created ' + folderPath);
@@ -34,7 +36,7 @@ if (shell.test('-e', folderPath)) {
 
 //check if scss folder exists
 if (shell.test('-e', scssFolderPath)) {
-	shell.echo('Sorry, the folder already exists ' + scssFolderPath);
+	shell.echo('The folder already exists ' + scssFolderPath);
 } else {
 	shell.mkdir(scssFolderPath);
 	shell.echo('Created ' + scssFolderPath);
@@ -42,7 +44,7 @@ if (shell.test('-e', scssFolderPath)) {
 
 //check if scss file exists
 if (shell.test('-e', scssFilePath)) {
-	shell.echo('Sorry, the folder already exists ' + scssFilePath);
+	shell.echo('The folder already exists ' + scssFilePath);
 } else {
 	shell.touch(scssFilePath);
 	shell.cat(scssTemplate).to(scssFilePath);
@@ -51,9 +53,9 @@ if (shell.test('-e', scssFilePath)) {
 	shell.echo('Created ' + scssFilePath);
 }
 
-//check if iterface file exists
+//check if interface file exists
 if (shell.test('-e', interfaceFilePath)) {
-	shell.echo('Sorry, the folder already exists ' + interfaceFilePath);
+	shell.echo('The folder already exists ' + interfaceFilePath);
 } else {
 	shell.touch(interfaceFilePath);
 	shell.cat(interfaceTemplate).to(interfaceFilePath);
@@ -61,9 +63,19 @@ if (shell.test('-e', interfaceFilePath)) {
 	shell.echo('Created ' + interfaceFilePath);
 }
 
+//check if enum file exists
+if (shell.test('-e', enumFilePath)) {
+	shell.echo('The folder already exists ' + enumFilePath);
+} else {
+	shell.touch(enumFilePath);
+	shell.cat(enumTemplate).to(enumFilePath);
+	shell.sed('-i', '<Pattern-Name>', patternname, enumFilePath);
+	shell.echo('Created ' + enumFilePath);
+}
+
 //check if pattern ts file exists
 if (shell.test('-e', pattFilePath)) {
-	shell.echo('Sorry, the folder already exists ' + pattFilePath);
+	shell.echo('The folder already exists ' + pattFilePath);
 } else {
 	shell.touch(pattFilePath);
 	shell.cat(pattTemplate).to(pattFilePath);
@@ -75,7 +87,7 @@ if (shell.test('-e', pattFilePath)) {
 
 //check if pattern API file exists
 if (shell.test('-e', pattAPIFilePath)) {
-	shell.echo('Sorry, the folder already exists ' + pattAPIFilePath);
+	shell.echo('The folder already exists ' + pattAPIFilePath);
 } else {
 	shell.touch(pattAPIFilePath);
 	shell.cat(apiTemplate).to(pattAPIFilePath);
@@ -85,7 +97,7 @@ if (shell.test('-e', pattAPIFilePath)) {
 
 //check if pattern config file exists
 if (shell.test('-e', pattConfigFilePath)) {
-	shell.echo('Sorry, the folder already exists ' + pattConfigFilePath);
+	shell.echo('The folder already exists ' + pattConfigFilePath);
 } else {
 	shell.touch(pattConfigFilePath);
 	shell.cat(pattConfigTemplate).to(pattConfigFilePath);
