61371b139137f9f01a57c4e3eafd305523742b0c
rug <rmfgoncalves@gmail.com>
2021-08-19 15:26:01 +0300

    Adding new IDisposable interface
    Code improvements

--- src/scripts/OSUIFramework/Pattern/Interface/IPattern.ts
+++ src/scripts/OSUIFramework/Pattern/Interface/IPattern.ts
@@ -3,19 +3,12 @@ namespace OSUIFramework.Interface {
 	/**
 	 * Defines the interface for OutSystemsUI Patterns
 	 */
-	export interface IPattern extends IBuilder, ISearchById {
+	export interface IPattern extends IBuilder, IDisposable, ISearchById {
 		isBuilt: boolean;
 		uniqueId: string;
 		widgetId: string;
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		changeProperty(propertyName: string, propertyValue: any): void;
-
-		destroy(): void;
-
-		finishBuild(): void;
-
-		// eslint-disable-next-line @typescript-eslint/member-ordering
-		UpdateExtendedClass(activeCssClass: string, newCssClass: string): void;
 	}
 }
