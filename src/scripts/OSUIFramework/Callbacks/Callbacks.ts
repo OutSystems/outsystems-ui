bd6b3bf27d68575f954d2d635c7f028267ea0705
rug <rmfgoncalves@gmail.com>
2021-08-27 16:32:23 +0300

    Adding interface IOpenable, ICallback & IRenderUpdate
    For better code separation and architecture.

--- src/scripts/OSUIFramework/Callbacks/Callbacks.ts
+++ src/scripts/OSUIFramework/Callbacks/Callbacks.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Callbacks {
+	export type OSGeneric = { (patternId, ...ags): void };
+
 	export type OSRatingSelectEvent = {
 		(ratingId: string, value: number): void;
 	};
