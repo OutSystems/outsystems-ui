3655ed11f49109e329d2b3e025e06b74f187a17f
José Rio <jose.rio@hotmail.com>
2021-08-30 15:22:38 +0100

    replaced repeated code related with ExtendedClass

--- src/scripts/OSUIFramework/GlobalEnum.ts
+++ src/scripts/OSUIFramework/GlobalEnum.ts
@@ -1,6 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.GlobalEnum {
 	/**
+	 * OutSystemsUI common properties
+	 */
+	export enum CommonPatternsProperties {
+		ExtendedClass = 'ExtendedClass',
+	}
+
+	/**
 	 * OutSystemsUI Data Blocks attribute values
 	 *
 	 * @export
