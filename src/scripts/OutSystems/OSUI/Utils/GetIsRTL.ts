3e21280931e571fa715a63e2af57b4d1a3eabdc6
BenOsodrac <bernardo.cardoso@outsystems.com>
2021-08-31 13:43:50 +0100

    Added Utils Documentation

--- src/scripts/OutSystems/OSUI/Utils/GetIsRTL.ts
+++ src/scripts/OutSystems/OSUI/Utils/GetIsRTL.ts
@@ -1,14 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OutSystems.OSUI.Utils {
 	/**
-	 *
+	 * Method that returns if the current screen is using RTL
 	 *
 	 * @export
 	 * @return {*}  {boolean}
 	 */
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	export function GetIsRTL(): boolean {
-		const isRTL = OSUIFramework.Helper.Style.ContainsClass(document.body, OSUIFramework.Constants.IsRTLClass);
-		return isRTL;
+		return OSUIFramework.Helper.Style.ContainsClass(document.body, OSUIFramework.Constants.IsRTLClass);
 	}
 }
--- src/scripts/OutSystems/OSUI/Utils/IsRunningAsNativeApp.ts
+++ src/scripts/OutSystems/OSUI/Utils/IsRunningAsNativeApp.ts
@@ -3,14 +3,14 @@ namespace OutSystems.OSUI.Utils {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	declare const window: any;
 	// eslint-disable-next-line @typescript-eslint/naming-convention
+
 	/**
-	 *
+	 * Method that returns if App is running as a Native App
 	 *
 	 * @export
 	 * @return {*}  {boolean}
 	 */
 	export function IsRunningAsNativeApp(): boolean {
-		const IsRunningAsNativeApp = window.cordova !== undefined && !IsRunningAsPWA();
-		return IsRunningAsNativeApp;
+		return window.cordova !== undefined && !IsRunningAsPWA();
 	}
 }
