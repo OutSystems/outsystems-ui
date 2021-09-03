58ca0c01b2a64b144e75e9c8a5a28495a87b83df
José Rio <jose.rio@hotmail.com>
2021-09-03 12:00:06 +0100

    relocate configs since they are equal for both patterns

--- src/scripts/OSUIFramework/Pattern/Progress/Bar/ProgressBarConfig.ts
+++ src/scripts/OSUIFramework/Pattern/Progress/Bar/ProgressBarConfig.ts
@@ -3,12 +3,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Patterns.Progress.Bar {
 	export class ProgressBarConfig extends Progress.ProgressConfiguration {
-		public Progress: number;
-		public ProgressColor: string;
-		public Shape: string;
-		public Thickness: number;
-		public TrailColor: string;
-
 		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 		constructor(config: any) {
 			super(config);
--- src/scripts/OSUIFramework/Pattern/Progress/Circle/ProgressCircleConfig.ts
+++ src/scripts/OSUIFramework/Pattern/Progress/Circle/ProgressCircleConfig.ts
@@ -3,12 +3,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Patterns.Progress.Circle {
 	export class ProgressCircleConfig extends Progress.ProgressConfiguration {
-		public Progress: number;
-		public ProgressColor: string;
-		public Shape: string;
-		public Thickness: number;
-		public TrailColor: string;
-
 		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 		constructor(config: any) {
 			super(config);
