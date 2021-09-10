437140df159b9efd3bb0294d9640639cd069198d
Joana Pereira <joana.borges@outsystems.com>
2021-09-10 15:03:13 +0100

    Floating Action first draft.

---
+++ src/scripts/OSUIFramework/Pattern/FloatingActions/FloatingActionsConfig.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace OSUIFramework.Patterns.FloatingActions {
+	export class FloatingActionsConfig extends AbstractConfiguration {
+		private _isInsideBottomBar: boolean;
+		private _isOpen: boolean;
+		public ExtendedClass: string;
+		public FloatingId: string;
+		public IsExpanded: boolean;
+		public IsHover: boolean;
+
+		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+		constructor(config: any) {
+			super(config);
+		}
+	}
+}
---
+++ src/scripts/OSUIFramework/Pattern/FloatingActions/IFloatingActions.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace OSUIFramework.Patterns.FloatingActions {
+	/**
+	 * Defines the interface for OutSystemsUI Floating Actions Pattern
+	 */
+	export interface IFloatingActions extends Interface.IPattern {}
+}
