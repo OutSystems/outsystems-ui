5f31072cced79ceb1deea173f35df199ea8cfe24
BenOsodrac <bernardo.cardoso@outsystems.com>
2021-10-01 15:08:47 +0100

    removed debugger

--- src/scripts/Providers/NoUiSlider/NoUiSlider.ts
+++ src/scripts/Providers/NoUiSlider/NoUiSlider.ts
@@ -225,8 +225,6 @@ namespace Providers.RangeSlider {
 
 		// Method to handle the pips options from the library
 		public handleRangePips(pipsStepParam: number, isUpdate: boolean): void {
-			// eslint-disable-next-line no-debugger
-			debugger;
 			let pipsValues = Math.floor(pipsStepParam);
 
 			// To avoid performance issues
