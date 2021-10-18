0231569844162eb99249d3af1f6846912e9c7e66
BenOsodrac <bernardo.cardoso@outsystems.com>
2021-10-18 15:17:00 +0100

    Fixed typo on Enum

--- src/scripts/Providers/NoUiSlider/Enum.ts
+++ src/scripts/Providers/NoUiSlider/Enum.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Providers.RangeSlider.Enum {
-	export enum NoUISsliderLabels {
+	export enum NoUISliderLabels {
 		Single = 'handle',
 		Lower = 'lower-handle',
 		Upper = 'upper-handle',
