844e23168c3753705e6c8a4612c64af5964e8148
bmarcelino-fe <brunoapmarcelino@gmail.com>
2021-11-22 12:19:35 +0000

    Update ManageAccessibility.ts

--- src/scripts/OSUIFramework/Helper/ManageAccessibility.ts
+++ src/scripts/OSUIFramework/Helper/ManageAccessibility.ts
@@ -3,6 +3,46 @@ namespace OSUIFramework.Helper {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	abstract class A11Y {
 		/**
+		 * Method that will disable the aria-atomic
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaAtomicFalse(element: HTMLElement): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Atomic, Constants.A11YAttributes.States.False);
+		}
+
+		/**
+		 * Method that will enable the aria-atomic
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaAtomicTrue(element: HTMLElement): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Atomic, Constants.A11YAttributes.States.True);
+		}
+
+		/**
+		 * Method that will define the aria-controls
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaControls(element: HTMLElement, targetId: string): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Controls, targetId);
+		}
+
+		/**
+		 * Method that will define the aria-describedby
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaDescribedBy(element: HTMLElement, targetId: string): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Describedby, targetId);
+		}
+
+		/**
 		 * Method that will enable the aria-expanded
 		 *
 		 * @param element
@@ -44,6 +84,88 @@ namespace OSUIFramework.Helper {
 		 */
 		public static AriaHiddenTrue(element: HTMLElement): void {
 			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Hidden, Constants.A11YAttributes.States.True);
+		}
+
+		/**
+		 * Method that will define the aria-label
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaLabel(element: HTMLElement, value: string): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Label, value);
+		}
+
+		/**
+		 * Method that will define the aria-labelledby
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaLabelledBy(element: HTMLElement, targetId: string): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.Labelledby, targetId);
+		}
+
+		/**
+		 * Method that will set the aria-live assertive
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaLiveAssertive(element: HTMLElement): void {
+			Helper.Attribute.Set(
+				element,
+				Constants.A11YAttributes.AriaLive.AttrName,
+				Constants.A11YAttributes.AriaLive.Assertive
+			);
+		}
+
+		/**
+		 * Method that will set the aria-live off
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaLiveOff(element: HTMLElement): void {
+			Helper.Attribute.Set(
+				element,
+				Constants.A11YAttributes.AriaLive.AttrName,
+				Constants.A11YAttributes.AriaLive.Off
+			);
+		}
+
+		/**
+		 * Method that will set the aria-live polite
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaLivePolite(element: HTMLElement): void {
+			Helper.Attribute.Set(
+				element,
+				Constants.A11YAttributes.AriaLive.AttrName,
+				Constants.A11YAttributes.AriaLive.Polite
+			);
+		}
+
+		/**
+		 * Method that will set the aria-value max
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaValueMax(element: HTMLElement, value: string): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.ValueMax, value);
+		}
+
+		/**
+		 * Method that will set the aria-value min
+		 *
+		 * @param element
+		 * @returns
+		 */
+		public static AriaValueMin(element: HTMLElement, value: string): void {
+			Helper.Attribute.Set(element, Constants.A11YAttributes.Aria.ValueMin, value);
 		}
 
 		/**
@@ -147,12 +269,3 @@ namespace OSUIFramework.Helper {
 		}
 	}
 }
-/*
-
-Helper.Attribute.Set(
-				this._notificationContent,
-				Constants.A11YAttributes.Role.AttrName,
-				Constants.A11YAttributes.Role.Alert
-			);
- 
-*/
