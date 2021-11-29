8154d7f22cd13eb8043b13dc17ac43c47631959c
rug <rmfgoncalves@gmail.com>
2021-11-29 17:32:57 +0300

    Adding documentation to interfaces

--- src/scripts/OSUIFramework/Pattern/Interface/IBuilder.ts
+++ src/scripts/OSUIFramework/Pattern/Interface/IBuilder.ts
@@ -2,11 +2,15 @@
 namespace OSUIFramework.Interface {
 	/**
 	 * Defines the interface for OutSystemsUI Patterns
+	 *
+	 * @export
+	 * @interface IBuilder
 	 */
 	export interface IBuilder {
 		/**
 		 * Build object, instantiating dependencies, and maniulating DOM when necessary
 		 *
+		 * @memberof IBuilder
 		 */
 		build(): void;
 	}
