7fc90437b2ecad772aa603857399a350bc60f5c7
rug <rmfgoncalves@gmail.com>
2021-12-01 21:17:30 +0300

    Adding documentation

--- src/scripts/OSUIFramework/Pattern/Sidebar/ISidebar.ts
+++ src/scripts/OSUIFramework/Pattern/Sidebar/ISidebar.ts
@@ -2,6 +2,11 @@
 namespace OSUIFramework.Patterns.Sidebar {
 	/**
 	 * Defines the interface for OutSystemsUI Sidebar Pattern
+	 *
+	 * @export
+	 * @interface ISidebar
+	 * @extends {Interface.IPattern}
+	 * @extends {Interface.ICallback}
 	 */
 	export interface ISidebar extends Interface.IPattern, Interface.ICallback {}
 }
