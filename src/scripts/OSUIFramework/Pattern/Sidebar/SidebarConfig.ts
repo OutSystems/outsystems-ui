4965c234a96c48d2ee33fdde6d91837201ae38ac
rug <rmfgoncalves@gmail.com>
2021-12-01 21:19:06 +0300

    Changing configuration mechanism

--- src/scripts/OSUIFramework/Pattern/Sidebar/SidebarConfig.ts
+++ src/scripts/OSUIFramework/Pattern/Sidebar/SidebarConfig.ts
@@ -1,14 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Patterns.Sidebar {
 	export class SidebarConfig extends AbstractConfiguration {
-		public Direction: string;
+		private _width: string;
+		public Direction: GlobalEnum.Direction;
 		public HasOverlay: boolean;
 		public IsOpen: boolean;
-		public Width: string;
 
-		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-		constructor(config: any) {
+		constructor(config: JSON) {
 			super(config);
+
+			if (this.Direction === undefined) {
+				this.Direction = GlobalEnum.Direction.Right;
+			}
+		}
+
+		public get Width(): string {
+			return this._width || '300px';
+		}
+		public set Width(width: string) {
+			this._width = width !== '' ? width : '300px';
 		}
 	}
 }
