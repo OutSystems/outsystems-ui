79ddf4edf6e59faebdd7a8c299a1a67207c07e6b
rug <rmfgoncalves@gmail.com>
2021-12-06 12:40:09 +0300

    Adding documentation and const with default width

--- src/scripts/OSUIFramework/Pattern/Sidebar/SidebarConfig.ts
+++ src/scripts/OSUIFramework/Pattern/Sidebar/SidebarConfig.ts
@@ -1,7 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Patterns.Sidebar {
 	export class SidebarConfig extends AbstractConfiguration {
+		private readonly _defaultWith = '300px';
 		private _width: string;
+
+		/** PUBLIC PROPERTIES **/
 		public Direction: GlobalEnum.Direction;
 		public HasOverlay: boolean;
 		public IsOpen: boolean;
@@ -14,11 +17,26 @@ namespace OSUIFramework.Patterns.Sidebar {
 			}
 		}
 
+		/**
+		 * Gets the value of the width (already fixed to the right value).
+		 *
+		 * @type {string}
+		 * @memberof SidebarConfig
+		 */
 		public get Width(): string {
-			return this._width || '300px';
+			return this._width;
 		}
+
+		/**
+		 * This setter enables us to have a single place of control of the width of the sidebar.
+		 * Like this, whenever the width is set, we can be sure that it's done having in consideration
+		 * the default value in case it's not provided.
+		 *
+		 * @memberof SidebarConfig
+		 */
 		public set Width(width: string) {
-			this._width = width !== '' ? width : '300px';
+			//Testing for '', undefined and null value.
+			this._width = width?.trim() ? width : this._defaultWith;
 		}
 	}
 }
