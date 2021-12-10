5ed0d2d77c2eeecde17debd14c1b31712c7a131d
rug <rmfgoncalves@gmail.com>
2021-12-10 14:34:00 +0300

    Adding documentation

--- src/scripts/OSUIFramework/Pattern/AbstractConfiguration.ts
+++ src/scripts/OSUIFramework/Pattern/AbstractConfiguration.ts
@@ -19,10 +19,29 @@ namespace OSUIFramework.Patterns {
 			}
 		}
 
+		/**
+		 * Method that helps to validate if a boolean is not undefined
+		 *
+		 * @protected
+		 * @param {(boolean | undefined)} value
+		 * @param {boolean} defaultValue
+		 * @return {*}  {boolean}
+		 * @memberof AbstractConfiguration
+		 */
 		protected validateBoolean(value: boolean | undefined, defaultValue: boolean): boolean {
 			return value !== undefined ? value : defaultValue;
 		}
 
+		/**
+		 * Method that helps to validate if a given value is within a range of values.
+		 *
+		 * @protected
+		 * @param {unknown} value
+		 * @param {unknown} defaultValue
+		 * @param {...unknown[]} args
+		 * @return {*}  {unknown}
+		 * @memberof AbstractConfiguration
+		 */
 		protected validateInRange(value: unknown, defaultValue: unknown, ...args: unknown[]): unknown {
 			if (value && args.includes(value)) {
 				return value;
@@ -30,15 +49,41 @@ namespace OSUIFramework.Patterns {
 			return defaultValue;
 		}
 
+		/**
+		 * Method that helps to validate if a string is not empty or undefined.
+		 *
+		 * @protected
+		 * @param {(string | undefined)} value
+		 * @param {string} defaultValue
+		 * @return {*}  {string}
+		 * @memberof AbstractConfiguration
+		 */
 		protected validateString(value: string | undefined, defaultValue: string): string {
 			return value && value.trim() ? value : defaultValue;
 		}
 
+		/**
+		 * Method that validates if a given property can be changed.
+		 *
+		 * @param {boolean} isBuilt
+		 * @param {string} key
+		 * @return {*}  {boolean}
+		 * @memberof AbstractConfiguration
+		 */
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		public validateCanChange(isBuilt: boolean, key: string): boolean {
 			return true;
 		}
 
+		/**
+		 * Method that assures that the values being set as configurations respect the defaults.
+		 *
+		 * @param {string} key
+		 * @param {unknown} value
+		 * @return {*}  {unknown}
+		 * @memberof AbstractConfiguration
+		 */
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		public validateDefault(key: string, value: unknown): unknown {
 			return value;
 		}
