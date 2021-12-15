5e9f2508e5ba0f7f3366454de5f68b87a0e7456c
BenOsodrac <bernardo.cardoso@outsystems.com>
2021-12-15 18:14:05 +0000

    Updated Tabs configs

--- src/scripts/OSUIFramework/Pattern/Tabs/TabsConfig.ts
+++ src/scripts/OSUIFramework/Pattern/Tabs/TabsConfig.ts
@@ -10,5 +10,39 @@ namespace OSUIFramework.Patterns.Tabs {
 		constructor(config: JSON) {
 			super(config);
 		}
+
+		public validateCanChange(isBuilt: boolean, key: string): boolean {
+			if (isBuilt) {
+				return key !== Enum.Properties.StartingTab;
+			}
+			return true;
+		}
+
+		public validateDefault(key: string, value: unknown): unknown {
+			let validatedValue = undefined;
+			switch (key) {
+				case Enum.Properties.TabsOrientation:
+					validatedValue = this.validateInRange(
+						value,
+						GlobalEnum.Orientation.Horizontal,
+						GlobalEnum.Orientation.Vertical
+					);
+					break;
+				case Enum.Properties.TabsVerticalPosition:
+					validatedValue = this.validateInRange(value, GlobalEnum.Direction.Left, GlobalEnum.Direction.Right);
+					break;
+				case Enum.Properties.JustifyHeaders:
+					validatedValue = this.validateBoolean(value as boolean, false);
+					break;
+				case Enum.Properties.Height:
+					validatedValue = this.validateString(value as string, 'auto');
+					break;
+				default:
+					validatedValue = super.validateDefault(key, value);
+					break;
+			}
+
+			return validatedValue;
+		}
 	}
 }
