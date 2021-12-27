37e658b6389fe99808b752061d3a7746d7479201
rug <rmfgoncalves@gmail.com>
2021-12-27 13:48:24 +0300

    Removing unecessary enums

--- src/scripts/OSUIFramework/Pattern/Search/Enum.ts
+++ src/scripts/OSUIFramework/Pattern/Search/Enum.ts
@@ -1,16 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Patterns.Search.Enum {
-	/**
-	 * Search Enum properties
-	 */
-	export enum Property {
-		ExtendedClass = 'ExtendedClass',
-	}
-
 	// Css Classes
 	export enum CssProperty {
 		InputPlaceholder = 'search-input',
-		LayoutNative = 'layout-native',
 		Pattern = 'search',
 		PatternIsOpen = 'open',
 		SearchGlass = 'search-glass',
