aa9549a19a403789b574ee05efa95300291ee82c
rug <rmfgoncalves@gmail.com>
2021-12-28 20:14:44 +0300

    Changing message

--- src/scripts/OSUIFramework/Pattern/RangeSlider/RangeSliderFactory.ts
+++ src/scripts/OSUIFramework/Pattern/RangeSlider/RangeSliderFactory.ts
@@ -23,7 +23,7 @@ namespace OSUIFramework.Patterns.RangeSlider.Factory {
 
 			default:
 				throw new Error(
-					`There is no  ${OSUIFramework.GlobalEnum.PatternsNames.RangeSlider}  of the ${provider} provider`
+					`There is no ${OSUIFramework.GlobalEnum.PatternsNames.RangeSlider} of the ${provider} provider`
 				);
 		}
 
