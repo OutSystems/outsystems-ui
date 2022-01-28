7c52b8c7f097af8bfb3221de7e8631f2c5b9e406
José Rio <jose.rio@hotmail.com>
2022-01-28 18:23:01 +0000

    Add a limit to the amount of starts that could be added.

--- src/scripts/OSUIFramework/Pattern/Rating/Enum.ts
+++ src/scripts/OSUIFramework/Pattern/Rating/Enum.ts
@@ -18,6 +18,7 @@ namespace OSUIFramework.Patterns.Rating.Enum {
 	 */
 	export enum Properties {
 		IsEdit = 'IsEdit',
+		MaxStars = 100,
 		RatingScale = 'RatingScale',
 		RatingValue = 'RatingValue',
 		Size = 'Size',
