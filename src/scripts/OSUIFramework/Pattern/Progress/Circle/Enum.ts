2bde9040f3372fd9fb998e122e104543ed13b0b9
Joana Pereira <joana.borges@outsystems.com>
2022-02-03 09:55:34 +0000

    Update class name

--- src/scripts/OSUIFramework/Pattern/Progress/Circle/Enum.ts
+++ src/scripts/OSUIFramework/Pattern/Progress/Circle/Enum.ts
@@ -4,8 +4,8 @@ namespace OSUIFramework.Patterns.Progress.Circle.Enum {
 	 * ProgressCircle Enum CssClasses
 	 */
 	export enum CssClass {
-		Progress = 'osui-progress-circle__progress-path',
-		Trail = 'osui-progress-circle__trail-path',
+		Progress = 'osui-progress-circle__container__progress-path',
+		Trail = 'osui-progress-circle__container__trail-path',
 	}
 
 	/**
