00890125a7e140767a4a070035125e13240c6067
José Rio <jose.rio@hotmail.com>
2022-02-09 16:53:19 +0000

    Test on Remove

--- src/scripts/Providers/Dropdown/OsUiComponents/DropdownAdvancedOpts.d.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-namespace Providers.Dropdown.OsUiComponents {
-	// Options List
-	export type DropdownAdvancedOpts = {
-		propName: string;
-	};
-}
