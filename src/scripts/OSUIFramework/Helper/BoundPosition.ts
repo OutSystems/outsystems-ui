884fb014298c45eecabd0dc11a70e17f746332ba
José Rio <jose.rio@hotmail.com>
2022-03-11 16:18:43 +0000

    Added a new useful helper to check if Boundaries Position of a given element.

--- src/scripts/OSUIFramework/Helper/BoundPosition.ts
+++ src/scripts/OSUIFramework/Helper/BoundPosition.ts
@@ -2,73 +2,121 @@
 namespace OSUIFramework.Helper {
 	export abstract class BoundPosition {
 		/**
+		 * Method that can be used to test if a given element is outside of a testAgainstElement boudaries
 		 *
-		 *
-		 * @param elem
-		 * @param viewportElem
-		 * @returns
+		 * @param element Element to check if is outside of boundaries
+		 * @param testAgainstElement Element where the boundaries will be tested
+		 * @param elementOffset Element Offset values to ba take in consideration
+		 * @returns {OutOfBoundaries}
 		 */
-		public static IsOutside(
+		public static Check(
 			element: HTMLElement,
-			offsetXValue = 0,
-			offsetYValue = 0,
-			viewportElement: HTMLElement = document.body
-		): void {
-			// Get element Bounds and sizes
-			const elementBound = element.getBoundingClientRect();
-			const elementHeight = elementBound.height + offsetYValue;
-			const elementWidth = elementBound.width + offsetXValue;
+			testAgainstElement: HTMLElement = document.body,
+			elementOffset: OffsetValues = { top: 0, right: 0, bottom: 0, left: 0 }
+		): OutOfBoundaries {
+			// Set element Bounds
+			const elementBounds = element.getBoundingClientRect();
+			// Set AgainstElement Bounds
+			const againstElementBounds = testAgainstElement.getBoundingClientRect();
 
-			// Get viewport element Bounds and sizes
-			const viewportElementBound = viewportElement.getBoundingClientRect();
-			const viewportElementHeight = viewportElementBound.height;
-			const viewportElementWidth = viewportElementBound.width;
-
-			// Check if it's out of the viewport on each side
-			const out = {
-				top: elementBound.top < viewportElementBound.top,
-				right: elementBound.right > viewportElementBound.right,
-				bottom: elementBound.bottom > viewportElementBound.bottom,
-				left: elementBound.left < viewportElementBound.left,
+			// Check if Element it's out of the AgainstElement boundaries
+			const isOut: OutOfBoundaries = {
+				top: elementBounds.top - elementOffset.top < againstElementBounds.top,
+				right: elementBounds.right + elementOffset.right > againstElementBounds.right,
+				bottom: elementBounds.bottom + elementOffset.bottom > againstElementBounds.bottom,
+				left: elementBounds.left - elementOffset.left < againstElementBounds.left,
 			};
 
-			// The item is out at top of viewportElem but has bigger height, so open it to bottom does not solve the issue!
-			if (out.top && elementHeight > viewportElementHeight) {
-				out.top = false;
-			}
-			// Since it has a smaller height than the viewportElem and doesn't fit at top position, check if fits at bottom!
-			else if (out.top && elementBound.top >= 0 && elementBound.top - viewportElementBound.top < elementHeight) {
-				out.top = false;
+			return isOut;
+		}
+
+		/**
+		 * Method that could be used to get a Recomended position from an element if it is ouside of boudaries of a given testAgainstElement
+		 *
+		 * @param element Element to check if is outside of boundaries
+		 * @param testAgainstElement Element where the boundaries will be tested
+		 * @param elementOffset Element Offset values to ba take in consideration
+		 * @returns {string | undefined} Suggested position (Based on GlobalEnum.Position)
+		 */
+		public static GetRecomendedPosition(
+			element: HTMLElement,
+			testAgainstElement: HTMLElement = document.body,
+			elementOffset: OffsetValues = { top: 0, right: 0, bottom: 0, left: 0 }
+		): string | undefined {
+			// Store the recomended position
+			let recomendedPosition = undefined;
+			// Set the Boundaries status of the given Element related with the AgainstElement boundaries
+			const checkOutOfBounds = BoundPosition.Check(element, testAgainstElement, elementOffset);
+
+			// If Element is not Outside of AgainstElement boundaries do not suggest a position!
+			if (Object.values(checkOutOfBounds).filter((val) => val).length === 0) {
+				return undefined;
 			}
 
-			// Same as above but in right position
-			if (out.right && elementWidth > viewportElementWidth) {
-				out.right = false;
-			}
-			// Since it has a smaller width than the viewportElem and doesn't fit at right position, check if fits at left!
-			else if (out.right && Math.abs(elementBound.left - viewportElementBound.left) < elementWidth) {
-				out.right = false;
+			// Set element Bounds
+			const elementBounds = element.getBoundingClientRect();
+			// Set AgainstElement Bounds
+			const againstElementBounds = testAgainstElement.getBoundingClientRect();
+
+			// If Element size (width or height) higher AgainstElement
+			if (
+				elementBounds.height > againstElementBounds.height ||
+				elementBounds.width > againstElementBounds.width
+			) {
+				// Doesn't matter if it's out of boundaries since it doesn't fit inside AgainstElement
+				checkOutOfBounds.top = false;
+				checkOutOfBounds.right = false;
+				checkOutOfBounds.bottom = false;
+				checkOutOfBounds.left = false;
 			}
 
-			// Same as above but regarding bottom position
-			if (out.bottom && elementHeight > viewportElementHeight) {
-				out.bottom = false;
-			}
-			// Since it has a smaller height than the viewportElem and doesn't fit at bottom position, check if fits at top!
-			else if (out.bottom && Math.abs(elementBound.bottom - viewportElementBound.bottom) < elementHeight) {
-				out.top = false;
+			// Is Out of Left boundary?
+			if (checkOutOfBounds.left) {
+				// Recomend open it at Right!
+				recomendedPosition = GlobalEnum.Position.Right;
 			}
 
-			// Same as above but regarding left position
-			if (out.left && elementWidth > viewportElementWidth) {
-				out.bottom = false;
-			}
-			// Since it has a smaller width than the viewportElem and doesn't fit at left position, check if fits at right!
-			else if (out.left && Math.abs(elementBound.right - viewportElementBound.right) < elementWidth) {
-				out.right = false;
+			// Is Out of Right boundary?
+			if (checkOutOfBounds.right) {
+				// Recomend open it at Left!
+				recomendedPosition = GlobalEnum.Position.Left;
 			}
 
-			console.log(out);
+			// Is Out of Left boundary?
+			if (checkOutOfBounds.top) {
+				// By default, recomend open it at Right!
+				recomendedPosition = GlobalEnum.Position.Bottom;
+
+				// Is Out of TopLeft boundary?
+				if (checkOutOfBounds.left) {
+					// Recomend open it at BottomRight!
+					recomendedPosition = GlobalEnum.Position.BottomRight;
+
+					// Is Out of TopRight boundary?
+				} else if (checkOutOfBounds.right) {
+					// Recomend open it at BottomLeft!
+					recomendedPosition = GlobalEnum.Position.BottomLeft;
+				}
+			}
+
+			// Is Out of Bottom boundary?
+			if (checkOutOfBounds.bottom) {
+				// By default, recomend open it at Top!
+				recomendedPosition = GlobalEnum.Position.Top;
+
+				// Is Out of BottomLeft boundary?
+				if (checkOutOfBounds.left) {
+					// Recomend open it at TopRight!
+					recomendedPosition = GlobalEnum.Position.TopRight;
+
+					// Is Out of BottomRight boundary?
+				} else if (checkOutOfBounds.right) {
+					// Recomend open it at TopLeft!
+					recomendedPosition = GlobalEnum.Position.TopLeft;
+				}
+			}
+
+			return recomendedPosition;
 		}
 	}
 }
