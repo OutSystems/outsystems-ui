e90b8b5b05fd638b54363d945369c7be8f1a30bd
João Ferreira <joao.manuel.ferreira@outsystems.com>
2022-03-17 14:42:18 +0000

    - Create SubmenuOpenOnHover API function; - Create OpenOnHover config; - Add necessary callbacks and listeners needed; - Create changeProperty method;

--- src/scripts/OSUIFramework/Pattern/Submenu/SubmenuConfig.ts
+++ src/scripts/OSUIFramework/Pattern/Submenu/SubmenuConfig.ts
@@ -3,8 +3,25 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSUIFramework.Patterns.Submenu {
 	export class SubmenuConfig extends AbstractConfiguration {
+		/** PUBLIC PROPERTIES **/
+		public OpenOnHover = false;
+
 		constructor(config: JSON) {
 			super(config);
+		}
+
+		public validateDefault(key: string, value: unknown): unknown {
+			let validatedValue = undefined;
+			switch (key) {
+				case Enum.Properties.OpenOnHover:
+					validatedValue = this.validateBoolean(value as boolean, false);
+					break;
+				default:
+					validatedValue = super.validateDefault(key, value);
+					break;
+			}
+
+			return validatedValue;
 		}
 	}
 }
