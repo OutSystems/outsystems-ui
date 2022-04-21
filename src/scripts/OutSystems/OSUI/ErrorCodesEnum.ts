4d0451fe181869af288684d86f61c236580c4e7b
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-04-21 12:04:20 +0100

    Update ErrorCodesEnum.ts

--- src/scripts/OutSystems/OSUI/ErrorCodesEnum.ts
+++ src/scripts/OutSystems/OSUI/ErrorCodesEnum.ts
@@ -23,7 +23,9 @@ namespace OutSystems.OSUI.ErrorCodes {
 	export const Notification = {
 		FailChangeProperty: 'OSUI-API-02001',
 		FailDispose: 'OSUI-API-02002',
-		FailRegisterCallback: 'OSUI-API-02003',
+		FailHide: 'OSUI-API-02003',
+		FailRegisterCallback: 'OSUI-API-02004',
+		FailShow: 'OSUI-API-02005',
 	};
 
 	export const SectionIndex = {
@@ -32,5 +34,87 @@ namespace OutSystems.OSUI.ErrorCodes {
 		FailDispose: 'OSUI-API-03003',
 		FailEnable: 'OSUI-API-03004',
 		FailRegisterCallback: 'OSUI-API-03005',
+	};
+
+	export const Accordion = {
+		FailChangeProperty: 'OSUI-API-04001',
+		FailCollapseAll: 'OSUI-API-04002',
+		FailDispose: 'OSUI-API-04003',
+		FailExpandAll: 'OSUI-API-04004',
+	};
+
+	export const AccordionItem = {
+		FailChangeProperty: 'OSUI-API-05001',
+		FailCollapseItem: 'OSUI-API-05002',
+		FailDispose: 'OSUI-API-05003',
+		FailExpandItem: 'OSUI-API-05004',
+		FailRegisterCallback: 'OSUI-API-05005',
+	};
+
+	export const Carousel = {
+		FailChangeProperty: 'OSUI-API-06001',
+		FailDispose: 'OSUI-API-06002',
+		FailGoTo: 'OSUI-API-06003',
+		FailNext: 'OSUI-API-06004',
+		FailPrevious: 'OSUI-API-06005',
+		FailRegisterCallback: 'OSUI-API-06006',
+		FailToggleDrag: 'OSUI-API-06007',
+		FailUpdate: 'OSUI-API-06008',
+	};
+
+	export const DatePicker = {
+		FailChangeProperty: 'OSUI-API-07001',
+		FailClear: 'OSUI-API-07002',
+		FailClose: 'OSUI-API-07003',
+		FailDispose: 'OSUI-API-07004',
+		FailOpen: 'OSUI-API-07005',
+		FailRedraw: 'OSUI-API-07006',
+		FailRegisterCallback: 'OSUI-API-07007',
+	};
+
+	export const FlipContent = {
+		FailChangeProperty: 'OSUI-API-08001',
+		FailDispose: 'OSUI-API-08002',
+		FailRegisterCallback: 'OSUI-API-08003',
+		FailShowBack: 'OSUI-API-08004',
+		FailShowFront: 'OSUI-API-08005',
+		FailToggle: 'OSUI-API-08006',
+	};
+
+	export const Progress = {
+		FailChangeProperty: 'OSUI-API-09001',
+		FailDispose: 'OSUI-API-09002',
+		FailProgressValue: 'OSUI-API-09003',
+	};
+
+	export const RangeSlider = {
+		FailChangeProperty: 'OSUI-API-10001',
+		FailDispose: 'OSUI-API-10002',
+		FailOnDragEnd: 'OSUI-API-10003',
+		FailRegisterCallback: 'OSUI-API-10004',
+	};
+
+	export const Sidebar = {
+		FailChangeProperty: 'OSUI-API-11001',
+		FailClose: 'OSUI-API-11002',
+		FailDispose: 'OSUI-API-11003',
+		FailOpen: 'OSUI-API-11004',
+		FailRegisterCallback: 'OSUI-API-11005',
+	};
+
+	export const Submenu = {
+		FailChangeProperty: 'OSUI-API-12001',
+		FailClose: 'OSUI-API-12002',
+		FailDispose: 'OSUI-API-12003',
+		FailOpen: 'OSUI-API-12004',
+		FailOpenOnHover: 'OSUI-API-12005',
+		FailUpdate: 'OSUI-API-12006',
+	};
+
+	export const Tooltip = {
+		FailChangeProperty: 'OSUI-API-13001',
+		FailClose: 'OSUI-API-13002',
+		FailDispose: 'OSUI-API-13003',
+		FailOpen: 'OSUI-API-13004',
 	};
 }
