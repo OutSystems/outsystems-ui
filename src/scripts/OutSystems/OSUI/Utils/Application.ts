42bcbd14063cc9dae734b1b958c0fcb2999d2f95
João Ferreira <joao.manuel.ferreira@outsystems.com>
2022-04-27 18:50:56 +0100

    - add ROU-2993 changes;

--- src/scripts/OutSystems/OSUI/Utils/Application.ts
+++ src/scripts/OutSystems/OSUI/Utils/Application.ts
@@ -3,20 +3,31 @@ namespace OutSystems.OSUI.Application {
 	export function SetExpandableExceptions(): void {
 		const layout = OSUIFramework.Helper.Dom.ClassSelector(document, 'layout');
 		const body = document.body;
-		const isExpandable = OSUIFramework.Helper.Dom.Styles.ContainsClass(layout, 'aside-expandable');
-		const nativeLayout = OSUIFramework.Helper.Dom.Styles.ContainsClass(layout, 'layout-native');
-		const isTabletLandscape =
-			OSUIFramework.Helper.Dom.Styles.ContainsClass(body, 'tablet') &&
-			OSUIFramework.Helper.Dom.Styles.ContainsClass(body, 'landscape');
-		const isDesktop = OSUIFramework.Helper.Dom.Styles.ContainsClass(body, 'desktop');
-		const isHideOnScroll = OSUIFramework.Helper.Dom.Styles.ContainsClass(layout, 'hide-header-on-scroll');
-		if (layout && isExpandable && ((nativeLayout && isTabletLandscape) || isDesktop)) {
-			Menu.ToggleSideMenu();
-		}
+		if (layout) {
+			const expandableMenuBehavior = OSUIFramework.Helper.Dom.Styles.ContainsClass(layout, 'aside-expandable');
+			const isLayoutNative = OSUIFramework.Helper.Dom.Styles.ContainsClass(layout, 'layout-native');
 
-		// Set StickyListener when HideHeaderOnScroll is used to adapt menu padding when Expandable
-		if (layout && nativeLayout && isHideOnScroll && isExpandable && (isTabletLandscape || isDesktop)) {
-			LayoutPrivate.SetStickyObserver();
+			if (expandableMenuBehavior && isLayoutNative) {
+				const deviceDetectionClasses =
+					(OSUIFramework.Helper.Dom.Styles.ContainsClass(body, 'tablet') &&
+						OSUIFramework.Helper.Dom.Styles.ContainsClass(body, 'landscape')) ||
+					OSUIFramework.Helper.Dom.Styles.ContainsClass(body, 'desktop');
+
+				// Check device detection classes
+				if (deviceDetectionClasses) {
+					const isHideOnScroll = OSUIFramework.Helper.Dom.Styles.ContainsClass(
+						layout,
+						'hide-header-on-scroll'
+					);
+					// Check if the menu is open and apply the correct menu visibility, when has Expandable behavior
+					Menu.ToggleSideMenu();
+
+					// Set StickyListener when HideHeaderOnScroll is used to adapt menu padding, when Expandable behavior
+					if (isHideOnScroll) {
+						LayoutPrivate.SetStickyObserver();
+					}
+				}
+			}
 		}
 	}
 }
