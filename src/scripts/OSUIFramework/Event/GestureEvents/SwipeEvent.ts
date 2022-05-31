af447f8722bcfa0790e1ce8cee2b7c4cc515c351
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2022-05-31 17:54:28 +0100

    comment typo

--- src/scripts/OSUIFramework/Event/GestureEvents/SwipeEvent.ts
+++ src/scripts/OSUIFramework/Event/GestureEvents/SwipeEvent.ts
@@ -14,7 +14,7 @@ namespace OSUIFramework.Event.GestureEvent {
 		private _swipeRightCallback: Callbacks.Generic;
 		private _swipeUpCallback: Callbacks.Generic;
 
-		// Distance threshold expected to be cosidered a swipe event
+		// Distance threshold expected to be considered a swipe event
 		private _threshold = 10;
 		// Velocity expected to be considered a swipe event
 		private _velocity = 0.3;
