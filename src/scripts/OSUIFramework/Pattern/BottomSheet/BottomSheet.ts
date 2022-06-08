f2d6b9f26eb97f0d6b8faadc891a2ba6a4310a8f
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2022-06-08 19:08:35 +0100

    Created focus trap class to solve code-smell

--- src/scripts/OSUIFramework/Pattern/BottomSheet/BottomSheet.ts
+++ src/scripts/OSUIFramework/Pattern/BottomSheet/BottomSheet.ts
@@ -73,20 +73,12 @@ namespace OSUIFramework.Patterns.BottomSheet {
 
 		// Focus on first focusable element on BottomSheet
 		private _focusBottomCallback(): void {
-			if (this._firstFocusableElement) {
-				this._firstFocusableElement.focus();
-			} else {
-				this._selfElem.focus();
-			}
+			Helper.FocusTrap.FocusOnFirstFocusableElement(this._firstFocusableElement, this._selfElem);
 		}
 
 		// Focus on last focusable element on BottomSheet
 		private _focusTopCallback(): void {
-			if (this._lastFocusableElement) {
-				this._lastFocusableElement.focus();
-			} else {
-				this._selfElem.focus();
-			}
+			Helper.FocusTrap.FocusOnLastFocusableElement(this._lastFocusableElement, this._selfElem);
 		}
 
 		// Add Focus Trap to Pattern
