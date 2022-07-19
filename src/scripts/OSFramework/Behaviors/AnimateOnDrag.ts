bfedecd7748646a0ce3da5e7e66ee94cc728ff85
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-07-19 11:57:36 +0100

    Change animations folder to behaviors folder and fix references

--- src/scripts/OSFramework/Behaviors/AnimateOnDrag.ts
+++ src/scripts/OSFramework/Behaviors/AnimateOnDrag.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-namespace OSFramework.Animations {
+namespace OSFramework.Behaviors {
 	// SpringAnimation type
 	type SpringAnimationKeyframes = {
 		frames: number;
