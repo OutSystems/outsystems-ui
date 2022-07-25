628016e71f32cabed55b1a63edc728ebd3443dd6
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-07-25 18:28:45 +0100

    Change temporarily the naming for the next changes across patterns

--- src/scripts/OSFramework/Callbacks/Callbacks.ts
+++ src/scripts/OSFramework/Callbacks/Callbacks.ts
@@ -1,13 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-namespace OSFramework.Callbacks {
-	//This type, enable us to mention a generic function that will be use.
-	//Receiving any number of parameters and having as output of an unknown type.
-	//This will allow us in the future not use "any" as a type of an function callback.
-	export type Generic = { (...args: unknown[]): unknown };
-
-	//This type is used to define callbacks that will be actions in OutSystems code side.
-	export type OSGeneric = { (patternId: string, ...args: unknown[]): void };
-
+namespace OSFramework.CallbacksOLD {
 	// DragEvent callback types
 	export type onGestureStart = { (x: number, y: number): void };
 	export type onGestureMove = { (x: number, y: number, offsetX: number, offsetY: number, evt: TouchEvent): void };
