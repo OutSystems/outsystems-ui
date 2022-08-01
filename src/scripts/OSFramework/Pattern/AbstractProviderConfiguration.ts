d3aacf518053f3c5fe791962445b8990fd085401
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2022-08-01 14:36:12 +0100

    asbract config

--- src/scripts/OSFramework/Pattern/AbstractProviderConfiguration.ts
+++ src/scripts/OSFramework/Pattern/AbstractProviderConfiguration.ts
@@ -10,6 +10,68 @@ namespace OSFramework.Patterns {
 	 * @extends {AbstractConfiguration}
 	 */
 	export abstract class AbstractProviderConfiguration extends AbstractConfiguration {
+		/**
+		 * Method to set the provider configs using extensibility
+		 *
+		 * @protected
+		 * @param {ProviderConfigs} defaultConfigs
+		 * @param {ProviderConfigs} newConfigs
+		 * @param {ProviderInfo} providerInfo
+		 * @return {*}  {ProviderConfigs}
+		 * @memberof AbstractProviderConfiguration
+		 */
+		protected setProviderConfig(
+			defaultConfigs: ProviderConfigs,
+			newConfigs: ProviderConfigs,
+			providerInfo: ProviderInfo
+		): ProviderConfigs {
+			for (const element in newConfigs) {
+				// Check if option passed is supported by the provider
+				if (providerInfo.supportedConfigs.hasOwnProperty(element) === false) {
+					console.warn(
+						`The config ${element} is not valid or supported by the ${providerInfo.name} provider, with the version ${providerInfo.version}`
+					);
+
+					delete newConfigs[element];
+				} else if (
+					providerInfo.supportedConfigs[element] !== null &&
+					providerInfo.supportedConfigs[element] !== undefined
+				) {
+					// Check for security vulnerabilites on the callbacks
+					if (
+						(typeof providerInfo.supportedConfigs[element] === 'object' &&
+							typeof newConfigs[element] === 'string') ||
+						(typeof providerInfo.supportedConfigs[element] === 'function' &&
+							typeof newConfigs[element] === 'string')
+					) {
+						console.warn(
+							`The config ${element} is not from the type expected by ${
+								providerInfo.name
+							} . The passed config is of type ${typeof newConfigs[
+								element
+							]}, but it should be ${typeof providerInfo.supportedConfigs[element]}!`
+						);
+
+						delete newConfigs[element];
+					}
+				}
+			}
+
+			// Merged passed configs with internal configs
+			const _finalConfigs = { ...defaultConfigs, ...newConfigs };
+
+			// Cleanning undefined properties
+			Object.keys(_finalConfigs).forEach(
+				(key) =>
+					(_finalConfigs[key] === undefined ||
+						_finalConfigs[key] === null ||
+						(Array.isArray(_finalConfigs[key]) && _finalConfigs[key].length === 0)) &&
+					delete _finalConfigs[key]
+			);
+
+			return _finalConfigs;
+		}
+
 		public abstract getProviderConfig(): void;
 	}
 }
