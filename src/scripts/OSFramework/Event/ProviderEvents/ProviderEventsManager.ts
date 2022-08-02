b0172ea44f53dbb794f98732b6de7f8d22b6193a
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2022-08-02 23:10:18 +0100

    added comments

--- src/scripts/OSFramework/Event/ProviderEvents/ProviderEventsManager.ts
+++ src/scripts/OSFramework/Event/ProviderEvents/ProviderEventsManager.ts
@@ -1,7 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.Event.ProviderEvents {
 	export class ProviderEventsManager implements IProviderEventManager {
+		// Map with the events added to provider and saved
 		private _eventsMap = new Map<string, IProviderEvent>();
+		// Map with the pending events to be added when provider is available
 		private _pendingEventsMap = new Map<string, IProviderEvent>();
 
 		public get eventsMap(): Map<string, IProviderEvent> {
@@ -12,19 +14,45 @@ namespace OSFramework.Event.ProviderEvents {
 			return this._pendingEventsMap;
 		}
 
+		/**
+		 * Method to add a pending event
+		 *
+		 * @param {string} eventName
+		 * @param {GlobalCallbacks.Generic} callback
+		 * @param {string} uniqueId
+		 * @memberof ProviderEventsManager
+		 */
 		public addPendingEvent(eventName: string, callback: GlobalCallbacks.Generic, uniqueId: string): void {
 			const newEvent = new ProviderEvent(callback, eventName, uniqueId);
 			this._pendingEventsMap.set(uniqueId, newEvent);
 		}
 
+		/**
+		 * Method to check if there're saved events
+		 *
+		 * @return {*}  {boolean}
+		 * @memberof ProviderEventsManager
+		 */
 		public hasEvents(): boolean {
 			return this._eventsMap.size > 0;
 		}
 
+		/**
+		 * Method to check if there're pending events
+		 *
+		 * @return {*}  {boolean}
+		 * @memberof ProviderEventsManager
+		 */
 		public hasPendingEvents(): boolean {
 			return this._pendingEventsMap.size > 0;
 		}
 
+		/**
+		 * Method to remove a pending event
+		 *
+		 * @param {string} uniqueId
+		 * @memberof ProviderEventsManager
+		 */
 		public removePendingEvent(uniqueId: string): void {
 			const event = this._pendingEventsMap.get(uniqueId);
 
@@ -33,6 +61,12 @@ namespace OSFramework.Event.ProviderEvents {
 			}
 		}
 
+		/**
+		 * Method to remove a saved event
+		 *
+		 * @param {string} uniqueId
+		 * @memberof ProviderEventsManager
+		 */
 		public removeSavedEvent(uniqueId: string): void {
 			const event = this._eventsMap.get(uniqueId);
 
@@ -41,6 +75,14 @@ namespace OSFramework.Event.ProviderEvents {
 			}
 		}
 
+		/**
+		 * Method to save an event
+		 *
+		 * @param {string} eventName
+		 * @param {GlobalCallbacks.Generic} callback
+		 * @param {string} uniqueId
+		 * @memberof ProviderEventsManager
+		 */
 		public saveEvent(eventName: string, callback: GlobalCallbacks.Generic, uniqueId: string): void {
 			let _newEvent;
 
