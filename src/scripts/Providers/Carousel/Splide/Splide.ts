1de2a44424ae08e7df0a16db21ca5dad2e8b76f0
Bernardo Cardoso <32780808+BenOsodrac@users.noreply.github.com>
2022-08-25 20:15:45 +0100

    ROU-3649: (v3) Improved onRender with autoplay (#503)
    * improved onRender with autoplay
    
    * added comment

--- src/scripts/Providers/Carousel/Splide/Splide.ts
+++ src/scripts/Providers/Carousel/Splide/Splide.ts
@@ -434,8 +434,9 @@ namespace Providers.Splide {
 				// Check if provider is ready
 				if (typeof this._provider === 'object') {
 					// Keep same position after update
-					if (this._currentIndex !== undefined) {
-						this.configs.StartingPosition = this._currentIndex;
+					// Check autoplay config, as that triggers the provider onChange and our onRender event, but doesn't udpate the _currentIndex property.
+					if (this._currentIndex !== undefined || this.configs.AutoPlay === true) {
+						this.configs.StartingPosition = this.provider.index;
 					}
 
 					this.updateCarousel();
