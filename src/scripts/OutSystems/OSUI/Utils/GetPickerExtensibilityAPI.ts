0485ddb4b35b16375054944e23b9f19c2740c375
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2022-10-04 15:41:53 +0100

    Added new helper api

---
+++ src/scripts/OutSystems/OSUI/Utils/GetPickerExtensibilityAPI.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/**
+ * Method to get the correct SetProvider Extensibility actions, based on widgetId
+ */
+namespace OutSystems.OSUI.Utils {
+	/**
+	 * Types of Provider Extensibility methods
+	 *
+	 * @export
+	 * @enum {number}
+	 */
+	export enum APIMethod {
+		SetProviderConfigs = 'SetProviderConfigs',
+		SetProviderEvent = 'SetProviderEvent',
+		UnsetProviderEvent = 'UnsetProviderEvent',
+	}
+
+	export function GetPickerExtensibilityAPI(
+		widgetId: string,
+		method: APIMethod
+	): OSFramework.GlobalCallbacks.Generic {
+		try {
+			OutSystems.OSUI.Patterns.DatePickerAPI.GetDatePickerItemById(widgetId);
+			return OutSystems.OSUI.Patterns.DatePickerAPI[method];
+		} catch (error) {
+			try {
+				OutSystems.OSUI.Patterns.TimePickerAPI.GetTimePickerItemById(widgetId);
+				return OutSystems.OSUI.Patterns.TimePickerAPI[method];
+			} catch (error) {
+				try {
+					OutSystems.OSUI.Patterns.MonthPickerAPI.GetMonthPickerItemById(widgetId);
+					return OutSystems.OSUI.Patterns.MonthPickerAPI[method];
+				} catch (error) {
+					console.warn(`WidgetId: ${widgetId} is not valid.`);
+				}
+			}
+		}
+	}
+}
