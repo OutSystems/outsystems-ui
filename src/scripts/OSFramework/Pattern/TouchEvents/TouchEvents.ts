dc84eae5e1faace6da29869a061f2559628e7ee9
José Rio <jose.rio@hotmail.com>
2022-10-11 12:07:49 +0100

    Add common abstract method to TouchEvents!

--- src/scripts/OSFramework/Pattern/TouchEvents/TouchEvents.ts
+++ src/scripts/OSFramework/Pattern/TouchEvents/TouchEvents.ts
@@ -157,6 +157,13 @@ namespace OSFramework.Patterns.TouchEvents {
 		}
 
 		/**
+		 * This method has no implementation on this pattern context!
+		 */
+		protected setA11YProperties(): void {
+			throw new Error('Method not implemented.');
+		}
+
+		/**
 		 * Sets the callbacks to be used in the pattern.
 		 *
 		 * @protected
