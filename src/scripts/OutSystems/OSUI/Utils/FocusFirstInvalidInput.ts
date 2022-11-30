8502e43a9b241b037959500c0dbd6d068b446951
bmarcelino-fe <brunoapmarcelino@gmail.com>
2022-11-30 17:23:32 +0000

    Add the features ScrollDelay and ScrollableElement to ScrollToElement API

--- src/scripts/OutSystems/OSUI/Utils/FocusFirstInvalidInput.ts
+++ src/scripts/OutSystems/OSUI/Utils/FocusFirstInvalidInput.ts
@@ -7,7 +7,7 @@ namespace OutSystems.OSUI.Utils.InvalidInputs {
 	 * @param {string} elementId
 	 * @param {boolean} isSmooth
 	 */
-	export function FocusFirstInvalidInput(elementId: string, isSmooth: boolean): string {
-		return OSFramework.Helper.InvalidInputs.FocusFirstInvalidInput(elementId, isSmooth);
+	export function FocusFirstInvalidInput(elementId: string, scrollableElement: string, isSmooth: boolean): string {
+		return OSFramework.Helper.InvalidInputs.FocusFirstInvalidInput(elementId, scrollableElement, isSmooth);
 	}
 }
--- src/scripts/OutSystems/OSUI/Utils/ScrollToElement.ts
+++ src/scripts/OutSystems/OSUI/Utils/ScrollToElement.ts
@@ -1,26 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 namespace OutSystems.OSUI.Utils {
-	export function ScrollToElement(ElementId: string, IsSmooth = true, OffSet = 0): void {
+	export function ScrollToElement(
+		ElementId: string,
+		ScrollableElement: string,
+		IsSmooth = true,
+		ScrollDelay?: number,
+		OffSet = 0
+	): void {
 		if (ElementId) {
 			const elementToScrollTo = document.getElementById(ElementId);
 
 			if (elementToScrollTo) {
-				// The app element that has the scroll and where the scrollTo wiull be applied
 				const activeScreen = OSFramework.Helper.Dom.ClassSelector(
 					document,
 					OSFramework.GlobalEnum.CssClassElements.ActiveScreen
 				);
+				// Set element that has the scroll and where the scrollTo will be applied
+				const scrollOnElement =
+					ScrollableElement !== ''
+						? elementToScrollTo.closest(OSFramework.Constants.Dot + ScrollableElement)
+						: activeScreen;
+				const scrollBehavior = IsSmooth
+					? OSFramework.GlobalEnum.ScrollBehavior.Smooth
+					: OSFramework.GlobalEnum.ScrollBehavior.Auto;
+				let top: number;
 
-				const header = OSFramework.Helper.Dom.ClassSelector(
-					document,
-					OSFramework.GlobalEnum.CssClassElements.Header
-				);
-
-				if (header) {
-					let top: number;
-					const scrollBehavior = IsSmooth
-						? OSFramework.GlobalEnum.ScrollBehavior.Smooth
-						: OSFramework.GlobalEnum.ScrollBehavior.Auto;
+				// Exception to be applied on custom patterns that has scrollable elements inside
+				if (scrollOnElement !== activeScreen) {
+					top =
+						elementToScrollTo.getBoundingClientRect().top -
+						scrollOnElement.getBoundingClientRect().top +
+						OffSet;
+				} else {
+					// Set the base value to apply on scroll
+					top = elementToScrollTo.getBoundingClientRect().top + OffSet;
 
 					const isHeaderFixed = OSFramework.Helper.Dom.ClassSelector(
 						document,
@@ -29,18 +42,24 @@ namespace OutSystems.OSUI.Utils {
 
 					// if fixed header, get the header height so that in the end it won't be covered by the header
 					if (isHeaderFixed) {
-						top = -header.offsetHeight + (elementToScrollTo.getBoundingClientRect().top + OffSet);
-					} else {
-						top = elementToScrollTo.getBoundingClientRect().top + OffSet;
-					}
+						const header = OSFramework.Helper.Dom.ClassSelector(
+							document,
+							OSFramework.GlobalEnum.CssClassElements.Header
+						);
 
-					if (activeScreen) {
-						activeScreen.scrollTo({
+						top = -header.offsetHeight + (elementToScrollTo.getBoundingClientRect().top + OffSet);
+					}
+				}
+
+				// Check if element exist to prevent errors
+				if (scrollOnElement) {
+					setTimeout(() => {
+						scrollOnElement.scrollTo({
 							top: top,
 							left: 0,
 							behavior: scrollBehavior,
 						});
-					}
+					}, ScrollDelay);
 				}
 			}
 		}
