8f99fcf86c06f256f174cdc7c5502c35495ac48e
José Rio <jose.rio@hotmail.com>
2022-12-06 14:49:05 +0000

    Added Note to the scss and updated scss file.

--- gulp/tasks/CreateScssFile.js
+++ gulp/tasks/CreateScssFile.js
@@ -16,6 +16,32 @@ function setProdEnvironment (cb) {
 	createScssFile(cb);
 }
 
+// Create a file note to give important context!
+function getNotesText () {
+	let notes = '';
+
+	notes += ""
+	notes = "/* ===============================================================================\n";
+	notes += `IMPORTANT NOTE:\n`;
+	notes += `• This file is generated automatically through npm scripts: \n`;
+	notes += `├── npm run create-osui-scss \n`;
+	notes += `|	└── Which will recreate this file as a Dev mode. \n`;
+	notes += `|\n`;
+	notes += `├── npm run build \n`;
+	notes += `|	└── When Prod mode is being start, it will be recreated before it's compilation. \n`;
+	notes += `|\n`;
+	notes += `└── npm run dev \n`;
+	notes += `	└── When Dev mode is being start, it will be recreated, than the compilation will occurs as usually. \n`;
+	notes += `\n`;
+	notes += `• In order to proper manage this file, use:\n`;
+	notes += `└── './gulp/ProjectSpecs/ScssStructure/#All.js' \n`;
+	notes += `\n`;
+	notes += `PS: This comment will not be visible at the compiled version!\n`;
+	notes +=	"=============================================================================== */\n";
+
+	return notes;
+}
+
 // Method used to Create SCSS file structure dynamically
 function createScssFile(cb) {
 	// Store the Section Index generated text
@@ -24,7 +50,7 @@ function createScssFile(cb) {
 	const partialsText = env === envType.production ? getPartialsList.textProd() : getPartialsList.textDev();
 
 	// Combine text to create the hole file
-	const newScssText = `${sectionIndexText}\n${partialsText}`;
+	const newScssText = `${getNotesText()}\n${sectionIndexText}\n${partialsText}`;
 
     // Update the SCSS file with the generated code!
     fs.writeFileSync(`./src/scss/OutSystemsUI.scss`, newScssText, 'utf8');
