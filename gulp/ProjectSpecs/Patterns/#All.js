5ece00151bf1db951bde3affe3ffa50838f6eef1
José Rio <jose.rio@hotmail.com>
2022-12-06 15:11:10 +0000

    Added month and time pickers to the scss.

--- gulp/ProjectSpecs/Patterns/#All.js
+++ gulp/ProjectSpecs/Patterns/#All.js
@@ -10,6 +10,7 @@ const dropdown = require('./Dropdown');
 const dropdownServerSideItem = require('./DropdownServerSideItem');
 const flipContent = require('./FlipContent');
 const gallery = require('./Gallery');
+const monthPicker = require('./MonthPicker');
 const notification = require('./Notification');
 const progress = require('./Progress');
 const rangeSlider = require('./RangeSlider');
@@ -22,6 +23,7 @@ const swipeEvents = require('./SwipeEvents');
 const tabs = require('./Tabs');
 const tabsContentItem = require('./TabsContentItem');
 const tabsHeaderItem = require('./TabsHeaderItem');
+const timePicker = require('./TimePicker');
 const tooltip = require('./Tooltip');
 const touchEvents = require('./TouchEvents');
 
@@ -36,31 +38,33 @@ const touchEvents = require('./TouchEvents');
 * For patterns with provider, structure can be slighly different in order to accomudate it's differences!
 **/
 const patterns = {
-	"accordion": accordion.info,
 	"accordion-item": accordionItem.info,
+	"accordion": accordion.info,
 	"animated-label": animatedLabel.info,	
 	"bottom-sheet": bottomSheet.info,	
 	"button-loading": buttonLoading.info,
 	"carousel": carousel.info,
 	"datepicker": datePicker.info,
-	"dropdown": dropdown.info,
 	"dropdown-serverside-item": dropdownServerSideItem.info,	
+	"dropdown": dropdown.info,
 	"flip-content": flipContent.info,	
 	"gallery": gallery.info,
+	"monthpicker": monthPicker.info,
 	"notification": notification.info,
 	"progress": progress.info,
 	"range-slider": rangeSlider.info,
 	"rating": rating.info,
-	"section-index": sectionIndex.info,
 	"section-index-item": sectionIndexItem.info,
+	"section-index": sectionIndex.info,
 	"sidebar": sidebar.info,
 	"submenu": submenu.info,
 	"swipe-events": swipeEvents.info,
-	"tabs": tabs.info,
 	"tabs-content-item": tabsContentItem.info,
 	"tabs-header-item": tabsHeaderItem.info,
+	"tabs": tabs.info,
+	"timepicker": timePicker.info,
 	"tooltip": tooltip.info,
-	"touch-events": touchEvents.info
+	"touch-events": touchEvents.info,
 };
 
 // Expose all patterns list!
