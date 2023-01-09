3323d13ba9b8079806e8297e46b787879c631191
Giuliana Silva <108938618+OS-giulianasilva@users.noreply.github.com>
2023-01-09 16:52:35 +0000

    Update OSUI version (#648)

--- gulp/ProjectSpecs/DefaultSpecs.js
+++ gulp/ProjectSpecs/DefaultSpecs.js
@@ -1,6 +1,6 @@
 // Store the default project specifications
 const specs = {
-    "version": "2.14.0",
+    "version": "2.15.0",
     "name": "OutSystems UI",
     "description": "",
     "url": "https://www.outsystems.com/outsystems-ui"
