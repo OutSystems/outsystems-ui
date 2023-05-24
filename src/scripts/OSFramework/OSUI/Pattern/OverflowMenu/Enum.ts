dc0e5e691bc65dd697193a12c124d24e907fb290
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-05-24 20:31:04 +0100

    commented Enum

--- src/scripts/OSFramework/OSUI/Pattern/OverflowMenu/Enum.ts
+++ src/scripts/OSFramework/OSUI/Pattern/OverflowMenu/Enum.ts
@@ -1,11 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.OSUI.Patterns.OverflowMenu.Enum {
+	/**
+	 * OverflowMenu Enum CSSClasses
+	 */
 	export enum CssClass {
 		Open = 'osui-overflow-menu--is-open',
 		Trigger = 'osui-overflow-menu__trigger',
 		Balloon = 'osui-overflow-menu__balloon',
 	}
 
+	/**
+	 * OverflowMenu Enum events
+	 */
 	export enum Events {
 		Initialized = 'Initialized',
 		OnMenuToggle = 'OnToggle',
