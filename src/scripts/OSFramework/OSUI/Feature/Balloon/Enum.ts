90df4162de9cd58e4bf9abdd0f58bb5dd8fb5247
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-05-25 09:46:36 +0100

    Added comments

--- src/scripts/OSFramework/OSUI/Feature/Balloon/Enum.ts
+++ src/scripts/OSFramework/OSUI/Feature/Balloon/Enum.ts
@@ -1,20 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.OSUI.Feature.Balloon.Enum {
-	// Css Classes
+	/**
+	 * Balloon Enum CSS Classes
+	 */
 	export enum CssClasses {
 		IsOpen = 'osui-balloon--is-open',
 		Pattern = 'osui-balloon',
 	}
 
+	/**
+	 * Balloon Enum CSS Variables
+	 */
 	export enum CssCustomProperties {
 		Shape = '--osui-balloon-shape',
 	}
 
+	/**
+	 * Balloon Enum events
+	 */
 	export enum Events {
 		Initialized = 'Initialized',
 		OnToggle = 'OnToggle',
 	}
 
+	/**
+	 * Balloon Enum Properties
+	 */
 	export enum Properties {
 		AnchorId = 'AnchorId',
 		BalloonPosition = 'BalloonPosition',
