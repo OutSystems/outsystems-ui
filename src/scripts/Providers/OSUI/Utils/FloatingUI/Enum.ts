98a1414bd45b2c4b3793d8ef4c3aa30420d2d1a5
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-05-25 10:21:03 +0100

    Added comment

--- src/scripts/Providers/OSUI/Utils/FloatingUI/Enum.ts
+++ src/scripts/Providers/OSUI/Utils/FloatingUI/Enum.ts
@@ -1,8 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Providers.OSUI.Utils.Enum {
+	/**
+	 * FloatingUI Enum CSS Variables
+	 */
 	export enum CssCustomProperties {
+		Offset = '--osui-floating-offset',
 		YPosition = '--osui-floating-position-y',
 		XPosition = '--osui-floating-position-x',
-		Offset = '--osui-floating-offset',
 	}
 }
