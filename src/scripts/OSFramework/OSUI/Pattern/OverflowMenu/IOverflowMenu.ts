a8aa374860107451c315639f6a4f562c8b58443d
giuliana <giuliana.silva@outsystems.com>
2023-07-11 16:56:15 +0100

    Add enable and disable capability to the OverflowMenu

--- src/scripts/OSFramework/OSUI/Pattern/OverflowMenu/IOverflowMenu.ts
+++ src/scripts/OSFramework/OSUI/Pattern/OverflowMenu/IOverflowMenu.ts
@@ -3,5 +3,8 @@ namespace OSFramework.OSUI.Patterns.OverflowMenu {
 	/**
 	 * Defines the interface for OutSystemsUI OverflowMenu Pattern
 	 */
-	export interface IOverflowMenu extends Interface.IPattern, Interface.IOpenable {}
+	export interface IOverflowMenu extends Interface.IPattern, Interface.IOpenable {
+		disable(): void;
+		enable(): void;
+	}
 }
