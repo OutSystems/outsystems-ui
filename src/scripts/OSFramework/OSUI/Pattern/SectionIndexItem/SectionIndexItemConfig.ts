380ee682f5444834c519a6269396d68ac9a8c970
bmarcelino-fe <brunoapmarcelino@gmail.com>
2023-07-13 18:01:41 +0100

    SectionIndex: Review the change property from that uses the validateDefault and validateCanChange methods before updates. Now this behavior is on the AbstractPattern and AbstractConfigurations.

--- src/scripts/OSFramework/OSUI/Pattern/SectionIndexItem/SectionIndexItemConfig.ts
+++ src/scripts/OSFramework/OSUI/Pattern/SectionIndexItem/SectionIndexItemConfig.ts
@@ -9,20 +9,5 @@ namespace OSFramework.OSUI.Patterns.SectionIndexItem {
 	 */
 	export class SectionIndexItemConfig extends AbstractConfiguration {
 		public ScrollToWidgetId: string;
-
-		/**
-		 * Method that will check if a given property (key) can be changed/updated!
-		 *
-		 * @param isBuilt True when pattern has been built!
-		 * @param key property name
-		 * @returns {boolean} boolean
-		 * @memberof  OSFramework.Patterns.SectionIndexItem.SectionIndexItemConfig
-		 */
-		public validateCanChange(isBuilt: boolean, key: string): boolean {
-			if (isBuilt) {
-				return key !== Enum.Properties.ScrollToWidgetId;
-			}
-			return true;
-		}
 	}
 }
