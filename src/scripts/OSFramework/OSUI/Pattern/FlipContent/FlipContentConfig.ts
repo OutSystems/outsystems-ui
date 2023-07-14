6c4e4bac28fb54634c78ab75d4944e65d4648daf
bmarcelino-fe <brunoapmarcelino@gmail.com>
2023-07-14 18:54:55 +0100

    FlipContent: Review the change property to prevent defaults of parameters

--- src/scripts/OSFramework/OSUI/Pattern/FlipContent/FlipContentConfig.ts
+++ src/scripts/OSFramework/OSUI/Pattern/FlipContent/FlipContentConfig.ts
@@ -14,5 +14,20 @@ namespace OSFramework.OSUI.Patterns.FlipContent {
 		constructor(config: JSON) {
 			super(config);
 		}
+
+		/**
+		 * Method that will check if a given property (key) can be changed/updated!
+		 *
+		 * @param isBuilt True when pattern has been built!
+		 * @param key property name
+		 * @returns {boolean} boolean
+		 * @memberof  OSFramework.Patterns.FlipContent.FlipContentConfig
+		 */
+		public validateCanChange(isBuilt: boolean, key: string): boolean {
+			if (isBuilt) {
+				return key !== Enum.Properties.IsFlipped;
+			}
+			return true;
+		}
 	}
 }
