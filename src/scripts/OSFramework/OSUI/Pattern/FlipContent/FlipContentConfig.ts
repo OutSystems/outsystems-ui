157bdf3f5a142382dd46d23545fcd1658df6a086
bmarcelino-fe <brunoapmarcelino@gmail.com>
2023-07-14 14:28:16 +0100

    FlipContent: Review the change property from that uses the validateDefault and validateCanChange methods before updates. Applied the correct comments for TypeDocs on TypeScript.

--- src/scripts/OSFramework/OSUI/Pattern/FlipContent/FlipContentConfig.ts
+++ src/scripts/OSFramework/OSUI/Pattern/FlipContent/FlipContentConfig.ts
@@ -1,26 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.OSUI.Patterns.FlipContent {
+	/**
+	 * Class that represents the custom configurations received by the FlipContent.
+	 *
+	 * @export
+	 * @class FlipContentConfig
+	 * @extends {AbstractConfiguration}
+	 */
 	export class FlipContentConfig extends AbstractConfiguration {
 		public FlipSelf: boolean;
 		public IsFlipped: boolean;
 
 		constructor(config: JSON) {
 			super(config);
-		}
-
-		/**
-		 * Method that will check if a given property (key) can be changed/updated!
-		 *
-		 * @param isBuilt True when pattern has been built!
-		 * @param key property name
-		 * @returns {boolean} boolean
-		 * @memberof  OSFramework.Patterns.FlipContent.FlipContentConfig
-		 */
-		public validateCanChange(isBuilt: boolean, key: string): boolean {
-			if (isBuilt) {
-				return key !== Enum.Properties.IsFlipped;
-			}
-			return true;
 		}
 	}
 }
