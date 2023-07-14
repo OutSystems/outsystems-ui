3569626d8c4aff5d73c9d9ee2877cbe1603bfd06
bmarcelino-fe <brunoapmarcelino@gmail.com>
2023-07-14 18:50:33 +0100

    Tabs: Review the change property to prevent defaults of parameters

--- src/scripts/OSFramework/OSUI/Pattern/Tabs/TabsConfig.ts
+++ src/scripts/OSFramework/OSUI/Pattern/Tabs/TabsConfig.ts
@@ -14,5 +14,56 @@ namespace OSFramework.OSUI.Patterns.Tabs {
 		public StartingTab: number;
 		public TabsOrientation: GlobalEnum.Orientation;
 		public TabsVerticalPosition: GlobalEnum.Direction;
+
+		/**
+		 * Method that will check if a given property (key) can be changed/updated!
+		 *
+		 * @param isBuilt True when pattern has been built!
+		 * @param key property name
+		 * @returns {boolean} boolean
+		 * @memberof  OSFramework.Patterns.Tabs.TabsConfig
+		 */
+		public validateCanChange(isBuilt: boolean, key: string): boolean {
+			if (isBuilt) {
+				return key !== Enum.Properties.StartingTab;
+			}
+			return true;
+		}
+
+		/**
+		 * Method that will check if a given property (key) value is the type expected!
+		 *
+		 * @param key property name
+		 * @param value value to be check
+		 * @returns {unknown} value
+		 * @memberof  OSFramework.Patterns.Tabs.TabsConfig
+		 */
+		public validateDefault(key: string, value: unknown): unknown {
+			let validatedValue = undefined;
+			switch (key) {
+				case Enum.Properties.TabsOrientation:
+					validatedValue = this.validateInRange(
+						value,
+						GlobalEnum.Orientation.Horizontal,
+						GlobalEnum.Orientation.Vertical
+					);
+					break;
+				case Enum.Properties.TabsVerticalPosition:
+					validatedValue = this.validateInRange(value, GlobalEnum.Direction.Left, GlobalEnum.Direction.Right);
+					break;
+				case Enum.Properties.ContentAutoHeight:
+				case Enum.Properties.JustifyHeaders:
+					validatedValue = this.validateBoolean(value as boolean, false);
+					break;
+				case Enum.Properties.Height:
+					validatedValue = this.validateString(value as string, GlobalEnum.CssProperties.Auto);
+					break;
+				default:
+					validatedValue = super.validateDefault(key, value);
+					break;
+			}
+
+			return validatedValue;
+		}
 	}
 }
