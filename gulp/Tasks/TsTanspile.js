c360a827e2ec634dfce80727d69fe9868dd857ee
José Rio <jose.rio@hotmail.com>
2023-12-05 15:47:25 +0000

    Update the way Ts is compiled in order to take care about different platforms.

--- gulp/Tasks/TsTanspile.js
+++ gulp/Tasks/TsTanspile.js
@@ -1,77 +1,135 @@
 const gulp = require('gulp');
 const {series} = require('gulp');
-
-const sourcemaps = require('gulp-sourcemaps');
 const fs = require('fs');
 const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
 
+const distFolder = './dist';
 const project = require('../ProjectSpecs/DefaultSpecs');
 
-const envType = {'development':'dev.', 'production':''};
-const distFolder = './dist';
+let defaultTsConfigText = '';
+let filesPath = {};
 
-let filePath = '';
-
-// Set as Development Mode
-function tsTranspileDev() {
-    return tsTranspile(envType.development);
+// Method that will handle the end of tsCompilation
+function onTsCompileFinish(platformType, cb) {
+    const pts = project.globalConsts.platforms;
+    if(platformType === pts[Object.keys(pts)[Object.keys(pts).length-1]]) {
+        cb();
+    }
 }
 
-// Set as Production Mode
-function tsTranspileProd() {
-    return tsTranspile(envType.production);
+// Update tsConfig after the compilation in order to ensure it will not be changed dynamically
+function rollBackTsConfigFile() {
+	// Update file with the default text!
+	fs.writeFileSync('tsconfig.json', defaultTsConfigText, 'utf8');
+    defaultTsConfigText = '';
 }
 
 // Compile TypeScript
-function tsTranspile(envMode) {
-    let tsResult;
+function tsTranspile(cb, envMode, platformType) {
+    // Set the default platformType
+    const pt = platformType !== undefined ? platformType : Object.keys(project.globalConsts.platforms)[0];
+    // Set filesPath accordingly as well
+    filesPath[pt] = `${distFolder}/${envMode === project.globalConsts.envType.production ? '' : envMode + "."}${project.globalConsts.platforms[pt]}.OutSystemsUI.js`;
+    // Update tsConfig file and do the Ts compilation accordingly
+    updateTsConfigFile(cb, envMode, project.globalConsts.platforms[pt]);
+    // Check if there is still any pending platform to tackle
+    if(pt !== Object.keys(project.globalConsts.platforms)[Object.keys(project.globalConsts.platforms).length-1]) {
+        tsTranspile(cb, envMode, Object.keys(project.globalConsts.platforms)[(Object.keys(project.globalConsts.platforms).indexOf(pt)+1)]);
+    }
+}
+
+// Method that will trigger the transpile of Ts according if it's development or production mode and platform type (O11 or ODC)
+async function tsTranspileBasedOnPlatform(cb, envMode, platformType) {
     let tsProject = ts.createProject('tsconfig.json', {
         outDir: distFolder,
-        declaration: envMode === envType.production ? true : false,
-        outFile: envMode + 'OutSystemsUI.js',
-    });
+        declaration: envMode === project.globalConsts.envType.production ? true : false,
+        outFile: `${envMode === project.globalConsts.envType.production ? '' : envMode + "."}${platformType}.OutSystemsUI.js`,
+    }); 
 
-    filePath = distFolder + "/" + envMode + 'OutSystemsUI.js';
-
-    if(envMode === envType.development) {
-        tsResult = tsProject
+    if(envMode === project.globalConsts.envType.development) {
+        tsProject
             .src()
             .pipe(sourcemaps.init())
             .pipe(tsProject()).js
             .pipe(sourcemaps.write("."))
-            .pipe(gulp.dest(distFolder));
+            .pipe(gulp.dest(distFolder))
+            .on('finish', () => {
+				onTsCompileFinish(platformType, cb);
+			});
     } else {
-        tsResult = tsProject
+        tsProject
             .src()
             .pipe(tsProject())
-            .pipe(gulp.dest(distFolder));
+            .pipe(gulp.dest(distFolder))
+            .on('finish', () => {
+                onTsCompileFinish(platformType, cb);
+			});
     }
 
-    return tsResult
+    // Rollback tsconfig file to the default state
+    if(defaultTsConfigText !== '') {
+        rollBackTsConfigFile();
+    }
 }
 
-// 
+// Set as Development Mode
+function tsTranspileDev(cb) {
+    tsTranspile(cb, project.globalConsts.envType.development);
+}
+
+// Set as Production Mode
+function tsTranspileProd(cb) {
+    tsTranspile(cb, project.globalConsts.envType.production);
+}
+
+// Add section info to the compiled files.
 function updateFwkInfo(cb) {    
-    // Set the Specifications text info
-    let specsInfo = '';
+    for(const pt in filesPath) {
+        // Set the Specifications text info
+        let specsInfo = '';
 
-    specsInfo += `/*!\n`;
-    specsInfo += `${project.info.name} ${project.info.version}\n`;
-    if (project.info.description !== '') {
-        specsInfo += `${project.info.description}\n`;
+        specsInfo += `/*!\n`;
+        specsInfo += `${project.info.name} ${project.info.version} • ${project.globalConsts.platforms[pt]} Platform\n`;
+        if (project.info.description !== '') {
+            specsInfo += `${project.info.description}\n`;
+        }
+        specsInfo += `${project.info.url}\n`;
+        specsInfo += `${project.info.gitHub}\n`;
+        specsInfo += `*/ \n`;
+
+        // Read file code
+        let code = fs.readFileSync(filesPath[pt], 'utf8');
+        // Update code
+        let updatedCode = specsInfo + code;
+        // Ensure code will be set properly before set the update.
+        setTimeout(() => {
+            // Update the existing file info with the new one!
+            fs.writeFileSync(filesPath[pt], updatedCode, 'utf8');
+        }, 0)
     }
-    specsInfo += `${project.info.url}\n`;
-    specsInfo += `${project.info.gitHub}\n`;
-    specsInfo += `*/ \n`;
-
-    // Read file code
-    let code = fs.readFileSync(filePath, 'utf8');
-    // Update code
-    let updatedCode = specsInfo + code;
-    // Update the existing file info with the new one!
-    fs.writeFileSync(filePath, updatedCode, 'utf8');
 
     cb();
+}
+
+// Method that will update the tsConfig file in order to set the exclude files under given platformType
+function updateTsConfigFile(cb, envMode, platformType) {
+	// Check if there are anything that should be ignored
+    if(project.globalConsts.excludeFromTsTranspile[platformType] !== undefined) {
+        // Get the text from tsConfig file
+        let code = fs.readFileSync('tsconfig.json', 'utf8');
+        // Store tsConfig text before updating it!
+        defaultTsConfigText = code;
+
+        // Create the new exclude text block
+	    const replaceInto = `"exclude": ${JSON.stringify(project.globalConsts.excludeFromTsTranspile[platformType])},`;
+        // Replace the text from the default one into the updated one!
+	    code = code.replace('"exclude": [],', replaceInto);
+        // Update file!
+	    fs.writeFileSync('tsconfig.json', code, 'utf8');
+	}
+
+    tsTranspileBasedOnPlatform(cb, envMode, platformType);
 }
 
 // TypeScript Transpile Task
