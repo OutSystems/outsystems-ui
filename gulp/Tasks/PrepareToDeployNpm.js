a975e2dfaf7172bd6c35bed01c7e6e0dd03a2a13
José Rio <jose.rio@hotmail.com>
2023-12-14 20:05:43 +0000

    Add the ability to update README file as well.

--- gulp/Tasks/PrepareToDeployNpm.js
+++ gulp/Tasks/PrepareToDeployNpm.js
@@ -24,7 +24,7 @@ const listOfPathFilesToRemove = [
     './typedoc.json',
 ];
 
-// Method that will update package.json file in order to remove unneeded stuff, once guld folder will also be removed!
+// Method that will update package.json file in order to remove unneeded stuff, once guld folder will also be removed
 function cleanPackageJsonFile() {
     let code = fs.readFileSync(`./package.json`, 'utf8');
     // Remove scripts section
@@ -35,6 +35,17 @@ function cleanPackageJsonFile() {
     code = code.replace(getTextToBeReplaced(code, '"devDependencies"', '}\n', true), '');
     // Update package.json file
     fs.writeFileSync(`./package.json`, JSON.stringify(jsonFix(code).data), 'utf8');
+}
+
+// Method that will update README.md file in order to remove uneeded text at the context of NPM package
+function cleanReadmeFile() {
+    let code = fs.readFileSync(`./README.md`, 'utf8');
+    // Remove scripts section
+    code = code.replace(getTextToBeReplaced(code, '### How to change this code?', '## Useful Links', false), '');
+    // Clean pending text
+    code = code.replace('### How to change this code?', '');
+    // Update package.json file
+    fs.writeFileSync(`./README.md`, code, 'utf8');
 }
 
 // Method that will return a piece of text between a given initial and end pieces of text
@@ -54,9 +65,10 @@ exports.prepareToDeployNpm = (cb) => {
     branchName.get().then((name) => {
         // Check if the curren branch is the expected one!
         if(name === prepareToDeployBranchName) {
+            // Update README.md
+            cleanReadmeFile();
             // Update package.json
             cleanPackageJsonFile();
-
             // Go through all the listOfPathFiles in order to remove them!
             for(const fpath of listOfPathFilesToRemove) {
                 // Remove current file
