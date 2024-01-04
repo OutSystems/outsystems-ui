8076ab1433b8caf5eaad62a0816ae98ed8c12a92
José Rio <jose.rio@hotmail.com>
2024-01-04 14:56:23 +0000

    Fixed issue - return proper content with scroll.

--- src/scripts/OSFramework/OSUI/Event/DOMEvents/Listeners/ScreenOnScroll.ts
+++ src/scripts/OSFramework/OSUI/Event/DOMEvents/Listeners/ScreenOnScroll.ts
@@ -1,6 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.OSUI.Event.DOMEvents.Listeners {
 	/**
+	 * Auxiliar method that will return the container where the scroll will be set
+	 *
+	 * @return {*}  {(HTMLElement | Document)}
+	 */
+	function scrollableScreenContainer(): HTMLElement | Document {
+		// Store the container element
+		let scrollableContainer: HTMLElement | Document = undefined;
+
+		// If native app, scrollable container will be the .content inside .active-scren
+		if (OSFramework.OSUI.Helper.DeviceInfo.IsNative) {
+			scrollableContainer = Helper.Dom.ClassSelector(
+				document,
+				`${GlobalEnum.CssClassElements.ActiveScreen} ${Constants.Dot}${GlobalEnum.CssClassElements.Content}`
+			);
+		} else {
+			// At non native apps, .active-screen is the one container with scroll
+			scrollableContainer = Helper.Dom.ClassSelector(document, GlobalEnum.CssClassElements.ActiveScreen);
+		}
+
+		// If any of the elements above has been find, probably user is using it's own laytout, in those cases body will be set as scrollable container.
+		return scrollableContainer !== undefined ? scrollableContainer : document.body;
+	}
+
+	/**
 	 * Class that represents the scroll on the active screen element.
 	 *
 	 * @export
@@ -9,7 +33,7 @@ namespace OSFramework.OSUI.Event.DOMEvents.Listeners {
 	 */
 	export class ScreenOnScroll extends AbstractListener<string> {
 		constructor() {
-			super( Helper.Dom.ClassSelector(document, GlobalEnum.CssClassElements.ActiveScreen), GlobalEnum.HTMLEvent.Scroll);
+			super(scrollableScreenContainer(), GlobalEnum.HTMLEvent.Scroll);
 			this.eventCallback = this._screenTrigger.bind(this);
 		}
 
