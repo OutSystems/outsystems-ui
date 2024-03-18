0aa6d53d19986e75bd63525fda3aa982976b7c70
UiComponentsBot <SA_Git_UiComponents@outsystems.com>
2024-03-18 23:57:47 +0000

    Updated into v2.18.5 [skip ci]

--- src/scripts/OSFramework/OSUI/Constants.ts
+++ src/scripts/OSFramework/OSUI/Constants.ts
@@ -136,7 +136,7 @@ namespace OSFramework.OSUI.Constants {
 	export const OSPlatform = '<->platformType<->';
 
 	/* OSUI Version */
-	export const OSUIVersion = '2.18.4';
+	export const OSUIVersion = '2.18.5';
 
 	/* Constant to be used across project as the zero value*/
 	export const ZeroValue = 0;
