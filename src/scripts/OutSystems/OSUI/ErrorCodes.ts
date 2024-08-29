07f396fe26c87ff94990b40e8b5e923e38b60249
José Rio <jose.rio@hotmail.com>
2024-08-29 17:56:46 +0100

    MonthPicker - Add new OnRender API to proper manage disabled status

--- src/scripts/OutSystems/OSUI/ErrorCodes.ts
+++ src/scripts/OutSystems/OSUI/ErrorCodes.ts
@@ -257,6 +257,7 @@ namespace OutSystems.OSUI.ErrorCodes {
 		FailUpdateInitialTime: 'OSUI-API-26013',
 		FailSetEditableInput: 'OSUI-API-26014',
 		FailUpdatePrompt: 'OSUI-API-26015',
+		FailOnRender: 'OSUI-API-26016',
 	};
 
 	export const MonthPicker = {
@@ -274,6 +275,7 @@ namespace OutSystems.OSUI.ErrorCodes {
 		FailSetLanguage: 'OSUI-API-27012',
 		FailUpdateInitialMonth: 'OSUI-API-27013',
 		FailUpdatePrompt: 'OSUI-API-27014',
+		FailOnRender: 'OSUI-API-27015',
 	};
 
 	//Error codes used on the context of the Utilities client actions
