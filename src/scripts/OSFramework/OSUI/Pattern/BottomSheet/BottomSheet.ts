e3c333273b6f8f1b8f5e9a2377f5788524cb8c52
gnbm <goncalo.martins@outsystems.com>
2025-07-04 17:52:09 +0100

    Update BottomSheet.ts

--- src/scripts/OSFramework/OSUI/Pattern/BottomSheet/BottomSheet.ts
+++ src/scripts/OSFramework/OSUI/Pattern/BottomSheet/BottomSheet.ts
@@ -82,7 +82,7 @@ namespace OSFramework.OSUI.Patterns.BottomSheet {
 
 		// Method to handle the creation of the GestureEvents
 		private _handleGestureEvents(): void {
-			if (Helper.DeviceInfo.IsNative) {
+			if (Helper.DeviceInfo.IsNative || Helper.DeviceInfo.IsTouch) {
 				// Create and save gesture event instance. Created here and not on constructor,
 				// as we need to pass element, only available after super.build()
 				this._gestureEventInstance = new Event.GestureEvent.DragEvent(this._bottomSheetHeaderElem);
