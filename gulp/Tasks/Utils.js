a5c54fba5d96653ced9c9fee9ec6203540e271a1
UiComponentsBot <sa_git_uicomponents@outsystems.com>
2026-02-06 12:06:15 +0000

    OK to Deploy into NPM. [skip ci]

--- gulp/Tasks/Utils.js
+++
@@ -1,7 +0,0 @@
-// Method that will return a piece of text between a given initial and end pieces of text
-exports.getTextToBeReplaced = (text, startOf, endWith, keepStartAndEnd) => {
-    const startIndex = text.indexOf(startOf) + (keepStartAndEnd ? 0 : startOf.length);
-	const endIndex = startIndex + (text.substring(startIndex, text.length).indexOf(endWith)) + (keepStartAndEnd ? endWith.length : 0);
-	const textBetween = text.substring(startIndex, endIndex);
-    return textBetween;
-}
