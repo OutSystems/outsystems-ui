b819e487ad00140f4ecc4a971dc35f5288baa636
José Rio <jose.rio@hotmail.com>
2026-08-04 18:51:59 +0100

    Removed Sass warning suppression in gulp task

--- gulp/Tasks/ScssTranspile.js
+++ gulp/Tasks/ScssTranspile.js
@@ -9,16 +9,6 @@ const rename = require("gulp-rename");
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 
-// Silence known, pre-existing Dart Sass deprecation warnings so the build output
-// stays clean and real errors stand out. 'import' was dropped once ROU-12911 migrated
-// all first-party SCSS to @use/@forward - keep it out so a regression isn't masked.
-// The rest are unrelated: legacy-js-api is gulp-sass's renderSync notice; global-builtin
-// and if-function are pre-existing, unrelated Sass API deprecations. Requires Dart Sass >= 1.80.
-const sassOptions = {
-	silenceDeprecations: ['global-builtin', 'if-function', 'legacy-js-api'],
-	quietDeps: true,
-};
-
 const project = require('../ProjectSpecs/DefaultSpecs');
 const distFolder = './dist';
 let watchScssThemes = 'src/scss/*.scss';
@@ -48,7 +38,7 @@ function scssTranspile(cb, envMode) {
     if(envMode === project.globalConsts.envType.development) {
         gulp.src(watchScssThemes)
 			.pipe(sourcemaps.init())
-			.pipe(sass(sassOptions).on('error', sass.logError))
+			.pipe(sass().on('error', sass.logError))
 			.pipe(postcss([postcssdc, postcssdd]))
 			.pipe(
 				autoprefixer({
@@ -64,7 +54,7 @@ function scssTranspile(cb, envMode) {
 			.pipe(gulp.dest(distFolder));
     } else {
         gulp.src(watchScssThemes)
-            .pipe(sass(sassOptions).on('error', sass.logError))
+            .pipe(sass().on('error', sass.logError))
             .pipe(postcss([postcssdc, postcssdd]))
             .pipe(autoprefixer({
                 overrideBrowserslist: ['last 10 versions']
